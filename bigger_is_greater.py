88a5a96c817659d10795af630e19da1049e28c1a
Randy <ryyuan@sfu.ca>
2017-01-23 15:08:55 -0800

    added a new algorithm

---
+++ bigger_is_greater.py
@@ -0,0 +1,20 @@
+for _ in range(int(input())):
+    s = input()
+    has_greater = False
+    for i in range(len(s)-1)[::-1]:
+        if ord(s[i]) < ord(s[i+1]):
+            for j in range(i+1,len(s))[::-1]:
+                if ord(s[i]) < ord(s[j]):
+                    lis = list(s)
+                    lis[i],lis[j]=lis[j],lis[i]
+                    print("".join(lis[:i+1]+lis[i+1:][::-1]))
+                    has_greater = True
+                if has_greater == True:
+                    break
+        if has_greater == True:
+            break
+    if has_greater == False:
+        print("no answer")
+
+# this algorithm counts the next smallest permutation of characters in the given string in O(n) time
+# source of algorithm: https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
