454c9a0501d0ad0f2551ef0afbf6a0bb09f67608
Randy <ryyuan@sfu.ca>
2017-02-23 17:08:30 -0800

    added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,20 @@
+_ = input()
+
+def sort_one(lis):
+    num = lis[-1]
+    leng = len(lis)
+    i = leng-1
+    while num<lis[i-1] and i>=1:
+        lis[i] = lis[i-1]
+        print(*lis)
+        lis[i-1]=num
+        i-=1
+    return lis
+
+lis = list(map(int,input().split()))
+for i in range(2,len(lis)+1):
+    lis = sort_one(lis[:i]) + lis[i:]
+    print lis
+
+# https://www.hackerrank.com/challenges/insertionsort2?h_r=next-challenge&h_v=zen
+# basic insertion sort
