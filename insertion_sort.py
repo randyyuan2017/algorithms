7c703705d2280adc97de15c1996ea50e7a173144
Randy <ryyuan@sfu.ca>
2017-02-23 17:11:45 -0800

    better insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,7 +6,6 @@ def sort_one(lis):
     i = leng-1
     while num<lis[i-1] and i>=1:
         lis[i] = lis[i-1]
-        print(*lis)
         lis[i-1]=num
         i-=1
     return lis
@@ -14,7 +13,7 @@ def sort_one(lis):
 lis = list(map(int,input().split()))
 for i in range(2,len(lis)+1):
     lis = sort_one(lis[:i]) + lis[i:]
-    print lis
+print(*lis)
 
-# https://www.hackerrank.com/challenges/insertionsort2?h_r=next-challenge&h_v=zen
+# https://www.hackerrank.com/challenges/correctness-invariant?h_r=next-challenge&h_v=zen
 # basic insertion sort
